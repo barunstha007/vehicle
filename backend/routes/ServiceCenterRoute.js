8fb8b57fb5e39e61b11f135b31e8ca2bc0ce6a88
Lokesh <lokesh.bajracharya55@gmail.com>
2019-11-28 22:07:13 +0545

    admin service center

--- backend/routes/ServiceCenterRoute.js
+++ backend/routes/ServiceCenterRoute.js
@@ -4,11 +4,15 @@ const User = require("../models/UserDetails.model");
 const mongoose = require("mongoose");
 const { check, validationResult } = require("express-validator");
 
-// @route   GET /serviceCenter
+// @route   GET /service-center
 // @desc    get admin Service Center
 // @access  @Admin
-router.get("/service-center", auth, async (req, res) => {
+router.get("/", auth, async (req, res) => {
   try {
+    //   Check admin role, 2 is admin
+    if (req.user.role !== 2) {
+      return res.status(400).json("You are not admin");
+    }
     // Check booking for current user
     const serviceCenter = await User.findOne({ user: req.user.id });
     // If no booking
@@ -23,57 +27,95 @@ router.get("/service-center", auth, async (req, res) => {
   }
 });
 
-//@router POST /bookings/
-//@desc add users booking
-//@access Private
-
-router.post("/", auth, async (req, res) => {
-  let bikebooking = await Bike.findOne({ user: req.user.id });
-  console.log(bikebooking.id);
-
-  const booking = {};
-  booking.bike = bikebooking.id;
-  booking.bookingStatus = true;
-
-  if (bikebooking) bike.bikeNumber = req.body.bikeNumber;
-
-  // Build profile Object
-  const bookingFields = {};
-
-  bookingFields.bike = req.user.id;
-
-  try {
-    // look for the profile with user req.user.id
-    let booking = await Booking.findOne({ user: req.user.id });
-
-    // if profile is found for the user
-    if (booking) {
-      console.log("Profile Found");
-      // For Update
-      mongoose.set("useFindAndModify", false);
-      booking = await Booking.findOneAndUpdate(
-        // Update profile in this id
-        { user: req.user.id },
-        { $set: bookingFields },
-        { new: true }
-      );
-
-      return res.json(booking);
-      // console.log('Booking Updated')
+//@router   POST /service-center/
+//@desc     add admins servicing center
+//@access   Admin
+router.post(
+  "/",
+  [
+    auth,
+    [
+      check("name", "Please enter your name")
+        .not()
+        .isEmpty(),
+      check("location", "Please enter a location")
+        .not()
+        .isEmpty(),
+      check("maxBookingDays", "Please enter max booking days")
+        .not()
+        .isEmpty()
+        .isNumeric(),
+      check("bookingLimit", "Please enter booking limit")
+        .not()
+        .isEmpty()
+        .isNumeric()
+    ]
+  ],
+  async (req, res) => {
+    // if user is customer, role == 3
+    if (req.user.role > 2) {
+      return res.status(400).json("You are not admin");
     }
 
-    console.log("Profile NOT Found");
-    // Else Create new profile
-    booking = new Booking(bookingFields);
-    console.log(booking); //CORRECT (has user id of current user)
-    await booking.save();
-    // res.json(booking)
-    console.log("Booking Created");
-    console.log(req.user);
-  } catch (err) {
-    console.error(err.message);
-    res.status(500).send("Server Error");
+    const errors = validationResult(req);
+    // If errors arenot empty || if there are errors
+    if (!errors.isEmpty) {
+      // take the error from express validation
+      return res.status(400).json({ errors: array() });
+    }
+
+    let serviceCenterUser;
+    const newServiceCenter = {};
+
+    // Admin can create and update only their profile
+    if (req.user.role == 2) {
+      // Check service center for user
+      serviceCenterUser = await User.findOne({ user: req.user.id });
+      newServiceCenter.admin = req.user.id;
+    }
+
+    if (req.body.name) newServiceCenter.name = req.body.name;
+    if (req.body.serviceLocation)
+      newServiceCenter.serviceLocation = req.body.serviceLocation;
+    if (req.body.maxBookingDays)
+      newServiceCenter.maxBookingDays = req.body.maxBookingDays;
+    if (req.body.bookingLimit)
+      newServiceCenter.bookingLimit = req.body.bookingLimit;
+    if (req.body.bikeNumber) bike.bikeNumber = req.body.bikeNumber;
+
+    try {
+      // look for the profile with user req.user.id
+      let booking = await Booking.findOne({ user: req.user.id });
+
+      // if profile is found for the user
+      if (booking) {
+        console.log("Profile Found");
+        // For Update
+        mongoose.set("useFindAndModify", false);
+        booking = await Booking.findOneAndUpdate(
+          // Update profile in this id
+          { user: req.user.id },
+          { $set: bookingFields },
+          { new: true }
+        );
+
+        return res.json(booking);
+        // console.log('Booking Updated')
+      }
+
+      console.log("Profile NOT Found");
+      // Else Create new profile
+      booking = new Booking(bookingFields);
+      console.log(booking); //CORRECT (has user id of current user)
+      await booking.save();
+      // res.json(booking)
+      console.log("Booking Created");
+      console.log(req.user);
+    } catch (err) {
+      console.error(err.message);
+      res.status(500).send("Server Error");
+    }
   }
-});
+);
 
 module.exports = router;
