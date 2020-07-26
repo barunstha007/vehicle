93af95f4948dbde66b1ac3e52928503f7132d638
Lokesh Bajracharya <lokesh.bajracharya55@gmail.com>
2020-07-26 12:46:51 +0545

    ui changes

---
+++ client/build/precache-manifest.5fe48d1b6cdecbfc4e1f40fd6df13480.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "803bab68b0416e8ffeb32288b315268e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "74867ac9e6c3804d8f25",
+    "url": "/static/css/2.1f830527.chunk.css"
+  },
+  {
+    "revision": "fa5e7e5940889188bb9b",
+    "url": "/static/css/main.fe7e0bb1.chunk.css"
+  },
+  {
+    "revision": "74867ac9e6c3804d8f25",
+    "url": "/static/js/2.cebc6320.chunk.js"
+  },
+  {
+    "revision": "fa5e7e5940889188bb9b",
+    "url": "/static/js/main.71e21201.chunk.js"
+  },
+  {
+    "revision": "92c89455b99cd109be4a",
+    "url": "/static/js/runtime-main.b5d1efea.js"
+  },
+  {
+    "revision": "c641d3da289557bd7e54e324156b115c",
+    "url": "/static/media/parts-banner.c641d3da.jpg"
+  }
+]);
